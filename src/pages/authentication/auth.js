903bf30660fcb5a7282f5f6a9d168e3953509e91
RogerBariles <rogerbariles@gmail.com>
2021-05-18 13:04:10 -0300

    se agrego ejemplo en pages authentication, login

--- src/pages/authentication/auth.js
+++ src/pages/authentication/auth.js
@@ -1,11 +1,39 @@
 //import './auth.scss';
 import React from 'react';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  { Redirect } from 'react-router-dom'
 
 function Auth() {
+
+    // funcionalidad 
+    const ProtectedComponent = () => {
+        return <Redirect to='authentication' />
+    }
 
     return (
         <div>
             <h1>LOGIN</h1>
+
+            <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                    <InputGroup.Text id="basic-addon3">
+                        NOMBRE USUARIO:
+                </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl id="basic-url" aria-describedby="basic-addon3" />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                    <InputGroup.Text id="basic-addon3">
+                        PASSWORD:
+                </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl type="password" id="basic-url" aria-describedby="basic-addon3" />
+            </InputGroup>
+
+            <Button variant="success" onClick={ProtectedComponent} >INGRESAR</Button>
         </div>
     );
 }
